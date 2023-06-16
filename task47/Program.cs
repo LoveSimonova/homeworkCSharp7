b2cec7ce9f95de5e52bc606c80fd27a5dd80b0e7
«SimLove» <simonolyuba@yandex.ru>
2023-06-16 18:02:31 +0300

    add task47

---
+++ task47/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+int GetRows()
+{
+    Console.WriteLine("Введите количество строк: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetColumns()
+{
+    Console.WriteLine("Введите количество столбцов: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] GetMass(int rows, int columns)
+{
+    double[,] massive = new double[rows,columns];
+    Random rnd = new Random();
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<columns;j++)
+        {
+            massive[i,j]=rnd.NextDouble();
+            Console.Write($"    "+ massive[i,j]);
+        }
+        Console.WriteLine();
+    }
+    return massive;
+}
+
+int m=GetRows();
+int n=GetColumns();
+GetMass(m,n);
